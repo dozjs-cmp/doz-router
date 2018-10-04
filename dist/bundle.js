8fabaaa9fadeef68f86c2ec257da25d96a4495c1
fabioricali <ricalifabio@gmail.com>
2018-10-04 12:14:15 +0200

    v1.2.2

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,4 @@
-// [DozRouter]  Build version: 1.2.1  
+// [DozRouter]  Build version: 1.2.2  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory(require("doz"));
